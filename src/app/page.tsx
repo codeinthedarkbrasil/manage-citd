1e8eec07007102e49835fbab7b66bcb555dc23ba
Marcos Oliveira <vmarcosp.pereira@gmail.com>
2023-06-21 20:42:51 -0300

    feat(events): added layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,42 @@
-import Link from 'next/link'
+import { Card, CardText, CardTitle } from "@/components";
+import { Button } from "@/components/button/Button";
 
 export default function Home() {
   return (
-    <div className="font-sans flex flex-col justify-between text-neutral-900">
-      <ul>
-        <li>
-          <Link href='/events'>Events</Link>
-          <Link href='/events/2023'>Code in the Dark 2023</Link>
-        </li>
-      </ul>
+    <div className="font-sans flex flex-col gap-4 justify-between text-neutral-900">
+      <div className="flex items-center justify-between">
+        <h1 className="text-title-sm text-neutral-900 font-bold">Eventos</h1>
+        <Button>
+          Novo evento
+        </Button>
+      </div>
+      <div className="grid grid-cols-3 gap-2">
+        <Card>
+          <div className="py-3 px-2">
+            <CardTitle>Code in The Dark 2022</CardTitle>
+            <CardText>
+              277 Participantes
+            </CardText>
+          </div>
+        </Card>
+        <Card>
+          <div className="py-3 px-2">
+            <CardTitle>Code in The Dark 2022</CardTitle>
+            <CardText>
+              277 Participantes
+            </CardText>
+          </div>
+        </Card>
+
+        <Card>
+          <div className="py-3 px-2">
+            <CardTitle>Code in The Dark 2022</CardTitle>
+            <CardText>
+              277 Participantes
+            </CardText>
+          </div>
+        </Card>
+      </div>
     </div>
   )
 }
