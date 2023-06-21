28b31b2c106ec50a7e22da0a254c8439850d1bc8
Marcos Oliveira <vmarcosp.pereira@gmail.com>
2023-06-21 20:54:19 -0300

    chore: removed unused types

--- src/app/events/[event]/layout.tsx
+++ src/app/events/[event]/layout.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query'
-import { PropsWithChildren } from '@/types/react'
+import { PropsWithChildren } from 'react'
 
 const queryClient = new QueryClient()
 
--- src/types/react.ts
+++
@@ -1,3 +0,0 @@
-export type PropsWithChildren = {
-  children: React.ReactNode
-}
