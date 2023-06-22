3cdad6a7c4ca90823b7a8840a5157151a44f6073
Pablo Henrique <pablohenriqueop@gmail.com>
2023-06-22 02:33:43 -0300

    feat: add link to participants list

--- src/app/events/[event]/page.tsx
+++ src/app/events/[event]/page.tsx
@@ -1,69 +1,76 @@
-'use client'
+"use client";
 
-import Link from 'next/link'
-import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
-import { TableBody } from './table-body'
-import { getParticipants, setSelectedParticipants, checkParticipant } from './participants/data-participants'
-import { getRandomInteger } from './get-random-integer'
+import Link from "next/link";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { TableBody } from "./table-body";
+import {
+  getParticipants,
+  setSelectedParticipants,
+  checkParticipant,
+} from "./participants/data-participants";
+import { getRandomInteger } from "./get-random-integer";
 
-type EventProps = {
+export type EventProps = {
   params: {
-    event: string
-  }
-}
+    event: string;
+  };
+};
 
 const generateGroups = async (ids: Set<string>) => {
-  return setSelectedParticipants(Array.from(ids))
-}
+  return setSelectedParticipants(Array.from(ids));
+};
 
 export default function Event({ params }: EventProps) {
-  const { event } = params
+  const { event } = params;
 
-  const queryClient = useQueryClient()
+  const queryClient = useQueryClient();
 
   const query = useQuery({
-    queryKey: ['participants', { event }],
+    queryKey: ["participants", { event }],
     queryFn: () => getParticipants(event),
-  })
+  });
 
   const generateGroupsMutation = useMutation({
     mutationFn: generateGroups,
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ['participants', { event }] })
+      queryClient.invalidateQueries({ queryKey: ["participants", { event }] });
     },
-  })
+  });
 
   const checkParticipantMutation = useMutation({
     mutationFn: checkParticipant,
-  })
+  });
 
   const handleCheckParticipant = (id: string) => {
-    checkParticipantMutation.mutate(id)
-  }
+    checkParticipantMutation.mutate(id);
+  };
 
-  const participants = query.data ?? []
-  console.count('Event rendered')
-  console.log('participants.length:', participants.length)
-  console.log('selected participants:', participants.filter(p => p.gonnaPlay))
+  const participants = query.data ?? [];
+  console.count("Event rendered");
+  console.log("participants.length:", participants.length);
+  console.log(
+    "selected participants:",
+    participants.filter((p) => p.gonnaPlay)
+  );
 
   const handleGenerateGroups = () => {
-    const selected = new Set<string>()
-    const checkedParticipants = participants.filter(p => p.wannaPlay)
+    const selected = new Set<string>();
+    const checkedParticipants = participants.filter((p) => p.wannaPlay);
     if (checkedParticipants.length < 16) {
-      console.log('Não tem a quantidade mínima selecionada')
-      return
+      console.log("Não tem a quantidade mínima selecionada");
+      return;
     }
 
     while (selected.size < 16) {
-      const rnd = getRandomInteger(participants.length - 1)
-      const participant = participants[rnd]
+      const rnd = getRandomInteger(participants.length - 1);
+      const participant = participants[rnd];
       if (participant.wannaPlay) {
-        selected.add(participants[rnd].id)
+        selected.add(participants[rnd].id);
       }
     }
-    console.log('all selected:', selected)
-    generateGroupsMutation.mutate(selected)
-  }
+    console.log("all selected:", selected);
+    generateGroupsMutation.mutate(selected);
+  };
 
   return (
     <main>
@@ -74,10 +81,15 @@ export default function Event({ params }: EventProps) {
             <button>Importar CSV</button>
           </li>
           <li>
-            <Link href={`/events/${event}/participants/new`}>Novo Participante</Link>
+            <Link href={`/events/${event}/participants/new`}>
+              Novo Participante
+            </Link>
           </li>
           <li>
             <button onClick={handleGenerateGroups}>Gerar chaves</button>
+          </li>
+          <li>
+            <Link href={`/events/${event}/participants/`}>Participants</Link>
           </li>
         </ul>
       </nav>
@@ -99,5 +111,5 @@ export default function Event({ params }: EventProps) {
         />
       </table>
     </main>
-  )
+  );
 }
