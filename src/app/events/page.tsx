e9606425b86d8abff45074524bdd7bc2afc12fb1
Marcos Oliveira <vmarcosp.pereira@gmail.com>
2023-06-26 18:31:17 -0300

    chore: removed lint-staged

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,42 +1,29 @@
-import Link from 'next/link'
+import Link from "next/link"
+import { Button, Card, CardText, CardTitle } from "@/components"
 
 export default function Events() {
   return (
-    <main>
-      <h1 className="font-sans text-neutral-900">Eventos</h1>
-      <button className="font-sans text-primary-100">Novo Evento</button>
-
-      <ul className="mt-3">
-        <li>
-          <EventItem
-            link='/events/2022'
-            title='Code in the Dark 2022'
-            description='277 Participantes'
-          />
-        </li>
-        <li>
-          <EventItem
-            link='/events/2023'
-            title='Code in the Dark 2023'
-            description='324 Participantes'
-          />
-        </li>
-      </ul>
-    </main>
-  )
-}
-
-type EventItemProps = {
-  link: string
-  title: string
-  description: string
-}
-
-function EventItem({ link, title, description }: EventItemProps) {
-  return (
-    <Link className="font-sans text-neutral-900" href={link}>
-      <h2>{title}</h2>
-      <p>{description}</p>
-    </Link>
+    <div className="font-sans flex flex-col gap-4 justify-between text-neutral-900">
+      <div className="flex items-center justify-between">
+        <h1 className="text-title-sm text-neutral-900 font-bold">Eventos</h1>
+        <Button>Novo evento</Button>
+      </div>
+      <div className="grid grid-cols-3 gap-2">
+        <Card>
+          <div className="py-3 px-2">
+            <CardTitle>Code in The Dark 2022</CardTitle>
+            <CardText>277 Participantes</CardText>
+          </div>
+        </Card>
+        <Link href="/events/2023">
+          <Card>
+            <div className="py-3 px-2">
+              <CardTitle>Code in The Dark 2022</CardTitle>
+              <CardText>277 Participantes</CardText>
+            </div>
+          </Card>
+        </Link>
+      </div>
+    </div>
   )
 }
