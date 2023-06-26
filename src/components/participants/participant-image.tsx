ba4ff933bbf0b256aca4cb93bc2068fe7f962fa1
Pablo Henrique <pablohenriqueop@gmail.com>
2023-06-25 23:06:09 -0300

    refactor: add wrapper to image

--- src/components/participants/participant-image.tsx
+++ src/components/participants/participant-image.tsx
@@ -14,19 +14,18 @@ export function ParticipantImage({
   alt,
   shouldHasLine,
 }: ParticipantImageProps) {
+  console.log("should has line", shouldHasLine)
   return (
-    <div
-      className={`w-[69px] h-[69px] border-2 border-primary-100 rounded-full overflow-hidden ${
-        shouldHasLine && afterClasses
-      }`}
-    >
-      <Image
-        src={src}
-        alt={alt}
-        width={69}
-        height={69}
-        className="w-full h-full object-cover relative z-10 transition-all duration-300 hover:scale-125"
-      />
+    <div className={`w-[69px] h-[69px]  ${shouldHasLine && afterClasses}`}>
+      <div className="overflow-hidden border-2 border-primary-100 rounded-full">
+        <Image
+          src={src}
+          alt={alt}
+          width={69}
+          height={69}
+          className="w-full h-full object-cover relative z-10 transition-all duration-300 hover:scale-125"
+        />
+      </div>
     </div>
   )
 }
