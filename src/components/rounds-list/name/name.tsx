aaf051ca937dd1e96b7078bb4e5cf0e0a3586baa
Pablo Henrique <pablohenriqueop@gmail.com>
2023-06-26 15:21:49 -0300

    feat: add event type

--- src/components/rounds-list/name/name.tsx
+++
@@ -1,9 +0,0 @@
-import { PropsWithChildren } from "react"
-
-export function Name({ children }: PropsWithChildren) {
-  return (
-    <span className="text-neutral-500 text-body-sm font-semibold leading-tight block">
-      {children}
-    </span>
-  )
-}
