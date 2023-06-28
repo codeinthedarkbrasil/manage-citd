132342e9f5301919995c5e9bdc487ab4f8264eb0
Gabriel Pinheiro <45916330+gabepinheiro@users.noreply.github.com>
2023-06-28 16:04:31 -0300

    chore(button): added variant text

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -5,25 +5,26 @@ import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
   `
-  inline-flex 
-  items-center 
-  justify-center 
-  rounded-md 
-  font-sans 
+  inline-flex
+  items-center
+  justify-center
+  rounded-md
+  font-sans
   text-[1.4rem]
-  font-bold 
-  transition-colors 
-  focus-visible:outline-none 
-  focus-visible:ring-2 
-  focus-visible:ring-ring 
-  focus-visible:ring-offset-2 
-  disabled:opacity-50 
-  disabled:pointer-events-none 
+  font-bold
+  transition-colors
+  focus-visible:outline-none
+  focus-visible:ring-2
+  focus-visible:ring-ring
+  focus-visible:ring-offset-2
+  disabled:opacity-50
+  disabled:pointer-events-none
   `,
   {
     variants: {
       variant: {
         primary: "bg-primary-100 text-neutral-100 hover:bg-primary-100/80",
+        text: "text-primary-100 hover:bg-primary-100 hover:text-neutral-100",
       },
       size: {
         sm: "h-[42px] px-2 rounded-1",
@@ -33,12 +34,12 @@ const buttonVariants = cva(
       variant: "primary",
       size: "sm",
     },
-  }
+  },
 )
 
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-  VariantProps<typeof buttonVariants> {
+    VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
 
@@ -46,14 +47,11 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ variant, size, asChild = false, children }, ref) => {
     const Component = asChild ? Slot : "button"
     return (
-      <Component
-        className={cn(buttonVariants({ variant, size }))}
-        ref={ref}
-      >
+      <Component className={cn(buttonVariants({ variant, size }))} ref={ref}>
         {children}
-      </Component >
+      </Component>
     )
-  }
+  },
 )
 Button.displayName = "Button"
 
