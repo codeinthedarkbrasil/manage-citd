57bd994be8771ec3aea51bb9c1b1c9cf37c0441e
vinibispo <vini.bispo015@gmail.com>
2023-07-05 11:19:31 -0300

    feat: create function to retrieve query params

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getQueryParams(request: Request): URLSearchParams {
+  const parsedUrl = new URL(request.url)
+  const queryString = parsedUrl.search
+  const queryParams = new URLSearchParams(queryString)
+  return queryParams
+}
