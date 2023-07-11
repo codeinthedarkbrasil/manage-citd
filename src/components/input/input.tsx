8567d5e77e671b8a8fe3da134a06b79e5f3cb5fb
Gabriel Pinheiro <45916330+gabepinheiro@users.noreply.github.com>
2023-07-11 11:42:07 -0300

    fix(components/input): removed left padding when not passing an icon

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,3 +1,5 @@
+import { cn } from "@/lib/utils"
+
 export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   icon?: React.ReactNode
@@ -6,17 +8,20 @@ export interface InputProps
 
 export function Input({ icon, label, id, ...props }: InputProps) {
   return (
-    <div>
+    <div className="w-full">
       <label htmlFor={id} className="sr-only">
         {label}
       </label>
-      <div className="relative flex items-center">
-        <div className="pointer-events-none absolute pl-[12px]">
-          {icon}
-        </div>
+      <div className="relative flex w-full items-center">
+        {!!icon && (
+          <div className="pointer-events-none absolute pl-[12px]">{icon}</div>
+        )}
         <input
           id={id}
-          className=" h-[34px] w-full rounded-[4px] bg-neutral-200 px-[12px] py-1 pl-[38px] text-[1.2rem] font-normal text-neutral-500 ring-offset-primary-100  transition-all placeholder:text-neutral-500 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
+          className={cn(
+            "h-[34px] w-full rounded-[4px] bg-neutral-200 px-[12px] py-1 font-sans text-[1.2rem] font-normal text-neutral-500 ring-offset-primary-100  transition-all placeholder:text-neutral-500 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
+            `${!!icon && "pl-[38px]"}`,
+          )}
           {...props}
         />
       </div>
