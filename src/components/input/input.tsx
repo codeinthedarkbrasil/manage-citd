93ab058672b1f7899d342e6ce578a9b534f8f8ff
Gabriel Pinheiro <45916330+gabepinheiro@users.noreply.github.com>
2023-07-11 12:18:02 -0300

    feat(components/input): added error state

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -4,27 +4,36 @@ export interface InputProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   icon?: React.ReactNode
   label?: string
+  error?: string
 }
 
-export function Input({ icon, label, id, ...props }: InputProps) {
+export function Input({ id, icon, label, error, ...props }: InputProps) {
   return (
-    <div className="w-full">
-      <label htmlFor={id} className="sr-only">
-        {label}
-      </label>
-      <div className="relative flex w-full items-center">
-        {!!icon && (
-          <div className="pointer-events-none absolute pl-[12px]">{icon}</div>
-        )}
-        <input
-          id={id}
-          className={cn(
-            "h-[34px] w-full rounded-[4px] bg-neutral-200 px-[12px] py-1 font-sans text-[1.2rem] font-normal text-neutral-500 ring-offset-primary-100  transition-all placeholder:text-neutral-500 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-            `${!!icon && "pl-[38px]"}`,
+    <div>
+      <div className="w-full">
+        <label htmlFor={id} className="sr-only">
+          {label}
+        </label>
+        <div className="relative flex w-full items-center">
+          {!!icon && (
+            <div className="pointer-events-none absolute pl-[12px]">{icon}</div>
           )}
-          {...props}
-        />
+          <input
+            id={id}
+            data-error={!!error}
+            className={cn(
+              "h-[34px] w-full rounded-[4px] bg-neutral-200 px-[12px] py-1 font-sans text-[1.2rem] font-normal text-neutral-500 ring-offset-primary-100  transition-all placeholder:text-neutral-500 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[error=true]:text-danger-100 data-[error=true]:ring-0 data-[error=true]:ring-offset-2 data-[error=true]:ring-offset-danger-100",
+              `${!!icon && "pl-[38px]"}`,
+            )}
+            {...props}
+          />
+        </div>
       </div>
+      {!!error && (
+        <span className="block pt-1 font-sans text-[1.4rem] text-danger-100">
+          {error}
+        </span>
+      )}
     </div>
   )
 }
