cdeb804344382cc75cd097f48133d3259d7162f4
Gabriel Pinheiro <45916330+gabepinheiro@users.noreply.github.com>
2023-07-11 14:27:17 -0300

    feat(register-participant-modal): added form validation

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,3 +1,5 @@
+import * as React from "react"
+
 import { cn } from "@/lib/utils"
 
 export interface InputProps
@@ -7,33 +9,41 @@ export interface InputProps
   error?: string
 }
 
-export function Input({ id, icon, label, error, ...props }: InputProps) {
-  return (
-    <div>
-      <div className="w-full">
-        <label htmlFor={id} className="sr-only">
-          {label}
-        </label>
-        <div className="relative flex w-full items-center">
-          {!!icon && (
-            <div className="pointer-events-none absolute pl-[12px]">{icon}</div>
-          )}
-          <input
-            id={id}
-            data-error={!!error}
-            className={cn(
-              "h-[34px] w-full rounded-[4px] bg-neutral-200 px-[12px] py-1 font-sans text-[1.2rem] font-normal text-neutral-500 ring-offset-primary-100  transition-all placeholder:text-neutral-500 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[error=true]:text-danger-100 data-[error=true]:ring-0 data-[error=true]:ring-offset-2 data-[error=true]:ring-offset-danger-100",
-              `${!!icon && "pl-[38px]"}`,
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ id, label, icon, error, children, ...props }, ref) => {
+    return (
+      <div>
+        <div className="w-full">
+          <label htmlFor={id} className="sr-only">
+            {label}
+          </label>
+          <div className="relative flex w-full items-center">
+            {!!icon && (
+              <div className="pointer-events-none absolute pl-[12px]">
+                {icon}
+              </div>
             )}
-            {...props}
-          />
+            <input
+              id={id}
+              data-error={!!error}
+              className={cn(
+                "h-[34px] w-full rounded-[4px] bg-neutral-200 px-[12px] py-1 font-sans text-[1.2rem] font-normal text-neutral-500 ring-offset-primary-100  transition-all placeholder:text-neutral-500 focus-visible:outline-none focus-visible:ring-0 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[error=true]:text-danger-100 data-[error=true]:ring-0 data-[error=true]:ring-offset-2 data-[error=true]:ring-offset-danger-100",
+                `${!!icon && "pl-[38px]"}`,
+              )}
+              ref={ref}
+              {...props}
+            />
+          </div>
         </div>
+        {!!error && (
+          <span className="block pt-1 font-sans text-[1.2rem] text-danger-100">
+            {error}
+          </span>
+        )}
       </div>
-      {!!error && (
-        <span className="block pt-1 font-sans text-[1.4rem] text-danger-100">
-          {error}
-        </span>
-      )}
-    </div>
-  )
-}
+    )
+  },
+)
+Input.displayName = "Input"
+
+export { Input }
--- src/components/register-participant-modal/modal.tsx
+++ src/components/register-participant-modal/modal.tsx
@@ -1,27 +1,62 @@
+"use client"
+
 import { Button } from "../button"
 import { Input } from "../input"
 import { ModalContent } from "../modal"
 import { User, AtSign, Github } from "lucide-react"
 
+import { useForm, SubmitHandler } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod"
+
+const formSchema = z.object({
+  name: z.string().min(1, { message: "Name is required" }),
+  email: z.string().email().min(1, { message: "Email is required" }),
+  github: z.string().min(1, { message: "Github user is required" }),
+})
+
+type FormSchemaValues = z.infer<typeof formSchema>
+
 export function RegisterParticipantModal() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormSchemaValues>({
+    resolver: zodResolver(formSchema),
+  })
+
+  const handleRegisterParticpant: SubmitHandler<FormSchemaValues> = (data) => {
+    console.log(data)
+  }
+
   return (
     <ModalContent>
       <div className="w-full min-w-[380px] px-[32px] py-[42px]">
         <h2 className="font-sans text-[1.6rem] font-semibold text-neutral-900">
           Novo Participante
         </h2>
-        <form className="mt-[24px] flex flex-col gap-3">
+        <form
+          className="mt-[24px] flex flex-col gap-3"
+          onSubmit={handleSubmit(handleRegisterParticpant)}
+        >
           <Input
             placeholder="Nome"
             icon={<User className="h-[16px] w-[16px] text-neutral-500" />}
+            error={errors.name?.message}
+            {...register("name")}
           />
           <Input
             placeholder="Email"
             icon={<AtSign className="h-[16px] w-[16px] text-neutral-500" />}
+            error={errors.email?.message}
+            {...register("email")}
           />
           <Input
             placeholder="Github"
             icon={<Github className="h-[16px] w-[16px] text-neutral-500" />}
+            error={errors.github?.message}
+            {...register("github")}
           />
 
           <Button>Confirmar</Button>
