d8905fa7e9f8032c3046fbc649eef0d50553599b
Fernando Daciuk <f.daciuk@gmail.com>
2023-07-12 19:28:58 -0300

    chore: update api directory

--- src/app/events/[event]/participants/data-participants.ts
+++ src/app/events/[event]/participants/data-participants.ts
@@ -1,7 +1,7 @@
 import { arrayOfParticipantsSchema, type Participant } from "@/shared/types"
 
 export async function getParticipants(event: string): Promise<Participant[]> {
-  const data = await fetch(`/events/${event}/participants/api`)
+  const data = await fetch(`/api/events/${event}/participants`)
   const participants = await data.json()
   return arrayOfParticipantsSchema.parse(participants)
 }
@@ -10,7 +10,7 @@ export async function getSelectedParticipants(
   event: string,
 ): Promise<Participant[]> {
   const data = await fetch(
-    `/events/${event}/participants/api/select-participants`,
+    `/api/events/${event}/participants/select-participants`,
   )
   const selectedParticipants = await data.json()
   return arrayOfParticipantsSchema.parse(selectedParticipants)
@@ -24,7 +24,7 @@ export async function setSelectedParticipants({
   event,
   ids,
 }: SetSelectedParticipantsInput) {
-  await fetch(`/events/${event}/participants/api/select-participants`, {
+  await fetch(`/api/events/${event}/participants/select-participants`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -45,7 +45,7 @@ export async function checkParticipant({
   event,
 }: CheckParticipantInput) {
   console.log("check participant checked?", checked)
-  await fetch(`/events/${event}/participants/${id}/api/check-participant`, {
+  await fetch(`/api/events/${event}/participants/${id}/check-participant`, {
     method: "POST",
     body: JSON.stringify({ checked }),
     headers: {
