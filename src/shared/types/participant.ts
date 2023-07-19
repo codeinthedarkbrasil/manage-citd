29328dbd91dff7dd70b056db1c0285549b31d41a
Fernando Daciuk <f.daciuk@gmail.com>
2023-07-19 12:30:38 -0300

    feat: set winner in database
    - create new type `ParticipantsInGroup` to make `groupId` not nullable
    - save winner in db

--- src/shared/types/participant.ts
+++ src/shared/types/participant.ts
@@ -8,12 +8,31 @@ const participantSchema = z.object({
   wannaPlay: z.coerce.boolean(),
   gonnaPlay: z.coerce.boolean(),
   winner: z.coerce.boolean(),
+})
+
+const groupIdSchema = z.object({
+  groupId: z.number(),
+})
+
+const groupIdNullableSchema = z.object({
   groupId: z.number().nullable(),
 })
 
-export const arrayOfParticipantsSchema = z.array(participantSchema)
+const participantOutsideGroupSchema = z.intersection(
+  participantSchema,
+  groupIdNullableSchema,
+)
+const participantInGroupSchema = z.intersection(
+  participantSchema,
+  groupIdSchema,
+)
 
-export type Participant = z.infer<typeof participantSchema>
+export const arrayOfParticipantsSchema = z.array(participantOutsideGroupSchema)
+export const arrayOfParticipantsInGroupSchema = z.array(
+  participantInGroupSchema,
+)
+
+export type Participant = z.infer<typeof participantOutsideGroupSchema>
 
 export const registerParticipantSchema = z.object({
   name: z.string().min(1, { message: "Name is required" }),
@@ -22,3 +41,5 @@ export const registerParticipantSchema = z.object({
 })
 
 export type RegisterParticipant = z.infer<typeof registerParticipantSchema>
+
+export type ParticipantInGroup = z.infer<typeof participantInGroupSchema>
