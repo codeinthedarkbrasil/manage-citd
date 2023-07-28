3717a17566b7ebec85be04ff6ea8696366a73d31
Fernando Daciuk <f.daciuk@gmail.com>
2023-07-28 13:47:34 -0300

    feat: hide participants from groups page

--- src/app/events/[event]/participants/page.tsx
+++ src/app/events/[event]/participants/page.tsx
@@ -11,44 +11,17 @@ import {
   RoundsList,
 } from "@/components"
 import { EventProps, Round } from "@/shared/types"
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
-import { RefreshCcw, ThumbsUp } from "lucide-react"
+import { useQuery } from "@tanstack/react-query"
 
-import {
-  getSelectedParticipants,
-  selectNewRandomPlayer,
-  setWinner,
-} from "./data-participants"
+import { getSelectedParticipants } from "./data-participants"
 import Link from "next/link"
 
 export default function Participants({ params }: EventProps) {
   const { event } = params
-  const queryClient = useQueryClient()
 
   const query = useQuery({
     queryKey: ["selected-participants", { event }],
     queryFn: () => getSelectedParticipants(event),
-  })
-
-  const winnerMutation = useMutation({
-    mutationFn: setWinner,
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: ["selected-participants", { event }],
-      })
-    },
-  })
-
-  const selectNewRandomPlayerMutation = useMutation({
-    mutationFn: selectNewRandomPlayer,
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        queryKey: ["participants", { event }],
-      })
-      queryClient.invalidateQueries({
-        queryKey: ["selected-participants", { event }],
-      })
-    },
   })
 
   const selectedParticipants = query.data ?? []
@@ -75,29 +48,6 @@ export default function Participants({ params }: EventProps) {
     { participants: [] },
   )
 
-  type HandleSetWinnerInput = {
-    userId: string
-    event: string
-    groupId: number
-  }
-
-  const handleSetWinner =
-    ({ userId, event, groupId }: HandleSetWinnerInput) =>
-    () => {
-      winnerMutation.mutate({ userId, event, groupId })
-    }
-
-  type HandleSelectNewRandomPlayerInput = {
-    userId: string
-    event: string
-    groupId: number
-  }
-  const handleSelectNewRandomPlayer =
-    ({ userId, event, groupId }: HandleSelectNewRandomPlayerInput) =>
-    () => {
-      selectNewRandomPlayerMutation.mutate({ userId, event, groupId })
-    }
-
   return (
     <section className="font-sans">
       <p className="text-body-xs leading-normal text-neutral-500 underline">
@@ -116,35 +66,14 @@ export default function Participants({ params }: EventProps) {
               {round.participants.map((participant, index) => (
                 <ParticipantItem key={participant.id}>
                   <div className="group relative h-[69px] w-[69px] cursor-pointer">
-                    <button
-                      className="absolute -top-1 right-0 z-aboveAll hidden h-[28px] w-[28px] items-center justify-center rounded-full bg-primary-100 group-hover:flex"
-                      onClick={handleSelectNewRandomPlayer({
-                        userId: participant.id,
-                        event,
-                        groupId: participant.groupId,
-                      })}
-                    >
-                      <RefreshCcw size={18} />
-                    </button>
-
                     <ParticipantImage
-                      src={`https://github.com/${participant.github}.png`}
-                      alt={`${participant.name} photo`}
+                      src="https://github.com/ghost.png"
+                      alt={`? photo`}
                       lined={index !== round.participants.length - 1}
                     />
-                    <button
-                      className="absolute -bottom-1 left-0 hidden h-[28px] w-[28px] items-center justify-center rounded-full bg-primary-100 transition-all group-hover:flex"
-                      onClick={handleSetWinner({
-                        userId: participant.id,
-                        event,
-                        groupId: participant.groupId,
-                      })}
-                    >
-                      <ThumbsUp size={18} />
-                    </button>
                   </div>
 
-                  <ParticipantName>{participant.name}</ParticipantName>
+                  <ParticipantName>??</ParticipantName>
                 </ParticipantItem>
               ))}
             </ParticipantsList>
