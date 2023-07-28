da44d823474fde933a2724c996bdb8bf598bb599
Evandro Ribeiro <ribeiroevandro@live.com>
2023-07-28 08:16:12 -0300

    feat: add link on round title

--- src/components/rounds-list/round-title.tsx
+++ src/components/rounds-list/round-title.tsx
@@ -1,9 +1,13 @@
 import { PropsWithChildren } from "react"
+import Link from "next/link"
 
 export function RoundTitle({ children }: PropsWithChildren) {
   return (
-    <span className="block text-body-sm font-semibold leading-tight text-neutral-500">
+    <Link
+      href="#"
+      className="block text-body-sm font-semibold leading-tight text-neutral-500 transition-colors hover:text-primary-100"
+    >
       {children}
-    </span>
+    </Link>
   )
 }
